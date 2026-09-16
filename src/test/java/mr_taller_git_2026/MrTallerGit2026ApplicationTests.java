ad95d429af3d33b088498e25731a148399148403
Mateo <mateo.rios2034@gmail.com>
2026-09-16 14:39:19 -0300

    chore: esqueleto Spring Boot API RESET desde start.spring.io

---
+++ src/test/java/mr_taller_git_2026/MrTallerGit2026ApplicationTests.java
@@ -0,0 +1,13 @@
+package mr_taller_git_2026;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MrTallerGit2026ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
