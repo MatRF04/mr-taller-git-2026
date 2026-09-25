e1deaa57d740f833b7bbe20cca250acc8d58e7d8
Mateo <mateo.rios2034@gmail.com>
2026-09-24 21:48:32 -0300

    feat: completar endpoint de indice

--- src/main/java/mr_taller_git_2026/controllers/IndexController.java
+++ src/main/java/mr_taller_git_2026/controllers/IndexController.java
@@ -8,6 +8,6 @@ public class IndexController {
 
     @GetMapping("/")
     public String index() {
-        return "hola amigo";
+        return "API de Minecraft de Mateo. Servicio funcionando.";
     }
 }
